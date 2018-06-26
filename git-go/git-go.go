24493751837a5b56b16656c91f49c09d41f39a83
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-06-25 22:48:42 -0700

    Add a gofmt check.
    Find all the Go files under the root and make sure they do not generate
    differences under gofmt -s.
    
    Hook up the new check to presubmit and check.

--- git-go/git-go.go
+++ git-go/git-go.go
@@ -23,10 +23,11 @@ func init() {
 Helpful additions for writing and maintaining Go code.
 
 Subcommands:
-  presubmit    : run "go test" and "go vet" over all packages
+  presubmit    : run "gofmt", "go test", and "go vet" over all packages
   test, test   : run "go test" over all packages
   vet          : run "go vet" over all packages
   lint         : run "golint" over all packages (if installed)
+  fmt, format  : run "gofmt -s" over all packages (if installed)
   check        : run all the above checks
 
   install-hook [subcommand]
@@ -75,12 +76,11 @@ func run() error {
 	}
 	args := flag.Args()
 	if len(args) == 1 && args[0] == "check" {
-		args = []string{"test", "vet", "lint"}
+		args = []string{"format", "test", "vet", "lint"}
 	}
 	var nerr int
 	for _, arg := range args {
 		err := func() error {
-
 			switch arg {
 			case "test", "tests":
 				return invoke(runTests(root))
@@ -92,13 +92,19 @@ func run() error {
 				return invoke(runLint(root))
 
 			case "presubmit":
+				fumpt := invoke(runFumpt(root))
 				test := invoke(runTests(root))
 				vet := invoke(runVet(root))
-				if test != nil {
+				if fumpt != nil {
+					return fumpt
+				} else if test != nil {
 					return test
 				} else if vet != nil {
 					return vet
 				}
+
+			case "fmt", "format":
+				return invoke(runFumpt(root))
 
 			default:
 				return fmt.Errorf("subcommand %q not understood", arg)
@@ -136,6 +142,24 @@ func runTests(path string) *exec.Cmd { return gocmd(path, "test", "-race", "./..
 func runVet(path string) *exec.Cmd { return gocmd(path, "vet", "./...") }
 
 func runLint(path string) *exec.Cmd { return runcmd("golint", path, "-set_exit_status", "./...") }
+
+func runFumpt(path string) *exec.Cmd {
+	const script = `find . -type f -name '*.go' -print0 \
+| xargs -0 gofmt -l -s \
+| grep .
+if [ $? -eq 0 ] ; then
+  echo "^ These files need formatting with go fmt"
+  exit 1
+fi
+`
+
+	cmd := exec.Command("/bin/sh", "-s", "--", "gofmt", "-l", "-s", "./...")
+	cmd.Stdin = strings.NewReader(script)
+	cmd.Stdout = os.Stderr
+	cmd.Stderr = os.Stderr
+	cmd.Dir = path
+	return cmd
+}
 
 func invoke(cmd *exec.Cmd) error {
 	fmt.Fprintf(out, "▷ \033[1;36m%s\033[0m\n", strings.Join(cmd.Args, " "))
