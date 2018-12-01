07ffb2e34539e7d3dce6e9fa5df40b3edf2a55ca
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-11-30 16:41:06 -0800

    git-go: Use -cpu > 1 when running with -race enabled.
    The race detector may miss some concurrency races if it is not run with truly
    parallel execution. Ensure this happens (whenever possible) by explicitly
    asking for multiple CPU when the race detector runs.

--- git-go/git-go.go
+++ git-go/git-go.go
@@ -137,7 +137,7 @@ func runcmd(bin, dir string, args ...string) *exec.Cmd {
 	return cmd
 }
 
-func runTests(path string) *exec.Cmd { return gocmd(path, "test", "-race", "./...") }
+func runTests(path string) *exec.Cmd { return gocmd(path, "test", "-race", "-cpu=1,2,4", "./...") }
 
 func runVet(path string) *exec.Cmd { return gocmd(path, "vet", "./...") }
 
