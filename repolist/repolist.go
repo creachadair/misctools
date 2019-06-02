70e88463afc40ba390810643f98a9f42ad21a6d9
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-06-01 21:54:54 -0700

    repolist: Add support for pagination.
    The various hosting providers have different strategies for sending pagination
    information, so rather than do something elegant I am taking advantage of the
    simple query structure, viz., page=N. We just keep asking for more pages until
    we don't get any new results.
    
    Despite the admonitions not to try to construct your own URL, this works fine
    and is much simpler than the recommended strategy.

--- repolist/repolist.go
+++ repolist/repolist.go
@@ -13,6 +13,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"bitbucket.org/creachadair/stringset"
@@ -24,10 +25,11 @@ var (
 	includeForks = flag.Bool("forks", false, "Include forks in listing")
 	doJSON       = flag.Bool("json", false, "Emit output as JSON objects")
 	authToken    = flag.String("auth", "", "Use this username:token to authenticate to the host")
+	doDebug      = flag.Bool("v", false, "Enable verbose logging")
 
 	hostMap = map[string]hostInfo{
 		"github": {
-			url: "https://api.github.com/users/{}/repos?per_page=100",
+			url: "https://api.github.com/users/{user}/repos?per_page=100&page={page}",
 			query: vql.Each(vql.Bind(map[string]vql.Query{
 				"url":    vql.Key("html_url"),
 				"desc":   vql.Or{vql.Key("description"), vql.Const("")},
@@ -35,7 +37,7 @@ var (
 			})),
 		},
 		"bitbucket": {
-			url: "https://api.bitbucket.org/2.0/repositories/{}?pagelen=100",
+			url: "https://api.bitbucket.org/2.0/repositories/{user}?pagelen=100&page={page}",
 			query: vql.Seq{
 				vql.Key("values"),
 				vql.Each(vql.Bind(map[string]vql.Query{
@@ -77,63 +79,77 @@ type hostInfo struct {
 }
 
 func (h hostInfo) fetch(user string) ([]string, error) {
-	query := strings.ReplaceAll(h.url, "{}", user)
-	req, err := http.NewRequest("GET", query, nil)
-	if err != nil {
-		return nil, fmt.Errorf("http request: %v", err)
-	}
+	var allNames []string
+	for page := 1; ; page++ {
+		query := strings.ReplaceAll(h.url, "{user}", user)
+		query = strings.ReplaceAll(query, "{page}", strconv.Itoa(page))
+		debug("Page %d query URL %q", page, query)
 
-	// Check for authorization credentials.
-	if *authToken != "" {
-		parts := strings.SplitN(*authToken, ":", 2)
-		if len(parts) != 2 {
-			return nil, errors.New("invalid auth token format")
+		req, err := http.NewRequest("GET", query, nil)
+		if err != nil {
+			return nil, fmt.Errorf("http request: %v", err)
 		}
-		req.SetBasicAuth(parts[0], parts[1])
-	}
 
-	// Issue the query and recover the JSON response blob.
-	rsp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("http get: %v", err)
-	}
-	defer rsp.Body.Close()
-
-	data, err := ioutil.ReadAll(rsp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("http body: %v", err)
-	}
-	if rsp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("http get: %s (%s)", rsp.Status, string(data))
-	}
-
-	// Decode the JSON response into structures and evaluate the host query to
-	// extract repository names and details.
-	var obj interface{}
-	if err := json.Unmarshal(data, &obj); err != nil {
-		return nil, fmt.Errorf("json unmarshal: %v", err)
-	}
-	v, err := vql.Eval(h.query, obj)
-	if err != nil {
-		return nil, fmt.Errorf("eval: %v", err)
-	}
-	var names []string
-	for _, elt := range v.([]interface{}) {
-		repo := elt.(map[string]interface{})
-		if repo["isFork"].(bool) && !*includeForks {
-			continue
-		}
-		if *doJSON {
-			bits, err := json.Marshal(repo)
-			if err != nil {
-				return nil, fmt.Errorf("render: %v", err)
+		// Check for authorization credentials.
+		if *authToken != "" {
+			parts := strings.SplitN(*authToken, ":", 2)
+			if len(parts) != 2 {
+				return nil, errors.New("invalid auth token format")
 			}
-			names = append(names, string(bits))
-		} else {
-			names = append(names, repo["url"].(string))
+			req.SetBasicAuth(parts[0], parts[1])
 		}
+
+		// Issue the query and recover the JSON response blob.
+		rsp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return nil, fmt.Errorf("http get: %v", err)
+		}
+		defer rsp.Body.Close()
+		debug("Response err=%v status=%v", err, rsp.StatusCode)
+
+		data, err := ioutil.ReadAll(rsp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("http body: %v", err)
+		}
+		debug("Read %d bytes of response data", len(data))
+		if rsp.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("http get: %s (%s)", rsp.Status, string(data))
+		}
+
+		// Decode the JSON response into structures and evaluate the host query to
+		// extract repository names and details.
+		var obj interface{}
+		if err := json.Unmarshal(data, &obj); err != nil {
+			return nil, fmt.Errorf("json unmarshal: %v", err)
+		}
+		v, err := vql.Eval(h.query, obj)
+		if err != nil {
+			return nil, fmt.Errorf("eval: %v", err)
+		}
+
+		var names []string
+		for _, elt := range v.([]interface{}) {
+			repo := elt.(map[string]interface{})
+			if repo["isFork"].(bool) && !*includeForks {
+				continue
+			}
+			if *doJSON {
+				bits, err := json.Marshal(repo)
+				if err != nil {
+					return nil, fmt.Errorf("render: %v", err)
+				}
+				names = append(names, string(bits))
+			} else {
+				names = append(names, repo["url"].(string))
+			}
+		}
+		if len(names) == 0 {
+			break // no more pages
+		}
+		allNames = append(allNames, names...)
+		debug("Adding %d repository names (%d total)", len(names), len(allNames))
 	}
-	return names, nil
+	return allNames, nil
 }
 
 func main() {
@@ -161,4 +177,10 @@ func main() {
 		all.Add(repos...)
 	}
 	fmt.Println(strings.Join(all.Elements(), "\n"))
+}
+
+func debug(msg string, args ...interface{}) {
+	if *doDebug {
+		log.Printf(msg, args...)
+	}
 }
