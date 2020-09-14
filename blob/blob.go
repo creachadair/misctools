2cfe30ff29aae21058d6932b91ac0fcec5ae0ba5
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-09-14 10:11:17 -0700

    Remove use of internal/vocab.

--- blob/blob.go
+++ blob/blob.go
@@ -10,6 +10,7 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"errors"
+	"flag"
 	"fmt"
 	"hash"
 	"io/ioutil"
@@ -19,6 +20,7 @@ import (
 	"strings"
 
 	"github.com/creachadair/badgerstore"
+	"github.com/creachadair/command"
 	"github.com/creachadair/ffs/blob"
 	"github.com/creachadair/ffs/blob/codecs/encrypted"
 	"github.com/creachadair/ffs/blob/codecs/zlib"
@@ -27,7 +29,6 @@ import (
 	"github.com/creachadair/ffs/blob/store"
 	"github.com/creachadair/getpass"
 	"github.com/creachadair/keyfile"
-	"github.com/creachadair/misctools/internal/vocab"
 )
 
 func init() {
@@ -43,166 +44,198 @@ func init() {
 }
 
 func main() {
-	v, err := vocab.New(filepath.Base(os.Args[0]), &tool{
-		KeyFile: os.Getenv("KEYFILE_PATH"),
-		CAS:     casGroup{Hash: "1"},
-	})
-	if err != nil {
-		log.Fatalf("Setup failed: %v", err)
-	}
-	ctx := context.Background()
-	if err := v.Dispatch(ctx, os.Args[1:]); err != nil {
+	if err := command.Execute(tool.NewContext(nil), os.Args[1:]); err != nil {
+		if errors.Is(err, command.ErrUsage) {
+			os.Exit(2)
+		}
 		log.Fatalf("Error: %v", err)
 	}
 }
 
-type getCmd struct{}
+var getCmd = &command.C{
+	Name:  "get",
+	Usage: "get <key>",
+	Help:  "Read a blob from the store",
 
-func (getCmd) Run(ctx context.Context, args []string) error {
-	if len(args) != 1 {
-		return errors.New("usage is: get <key>")
-	}
-	key, err := parseKey(args[0])
-	if err != nil {
-		return err
-	}
-	bs, err := storeFromContext(ctx)
-	if err != nil {
-		return err
-	}
-	data, err := bs.Get(ctx, key)
-	if err != nil {
-		return err
-	}
-	_, err = os.Stdout.Write(data)
-	return err
-}
-
-type putCmd struct {
-	Replace bool `flag:"replace,Replace an existing key"`
-}
-
-func (p putCmd) Run(ctx context.Context, args []string) error {
-	if len(args) == 0 || len(args) > 2 {
-		return errors.New("usage is: put <key> [<path>]")
-	}
-	key, err := parseKey(args[0])
-	if err != nil {
-		return err
-	}
-	bs, err := storeFromContext(ctx)
-	if err != nil {
-		return nil
-	}
-	data, err := readData(ctx, "put", args[1:])
-	if err != nil {
-		return err
-	}
-
-	return bs.Put(ctx, blob.PutOptions{
-		Key:     key,
-		Data:    data,
-		Replace: p.Replace,
-	})
-}
-
-type sizeCmd struct{}
-
-func (sizeCmd) Run(ctx context.Context, args []string) error {
-	if len(args) != 1 {
-		return errors.New("usage is: size <key>")
-	}
-	key, err := parseKey(args[0])
-	if err != nil {
-		return err
-	}
-	bs, err := storeFromContext(ctx)
-	if err != nil {
-		return err
-	}
-	size, err := bs.Size(ctx, key)
-	if err != nil {
-		return err
-	}
-	fmt.Println(size)
-	return nil
-}
-
-type delCmd struct{}
-
-func (delCmd) Run(ctx context.Context, args []string) error {
-	if len(args) != 1 {
-		return errors.New("usage is: delete <key>")
-	}
-	key, err := parseKey(args[0])
-	if err != nil {
-		return err
-	}
-	bs, err := storeFromContext(ctx)
-	if err != nil {
-		return err
-	}
-	del, ok := bs.(blob.Deleter)
-	if !ok {
-		return errors.New("store does not support deletion")
-	}
-	return del.Delete(ctx, key)
-}
-
-type listCmd struct {
-	Raw   bool   `flag:"raw,Print raw keys without hex encoding"`
-	Start string `flag:"start,List keys lexicographically greater than or equal to this"`
-}
-
-func (c listCmd) Run(ctx context.Context, args []string) error {
-	if len(args) != 0 {
-		return errors.New("usage is: list")
-	}
-	start, err := parseKey(c.Start)
-	if err != nil {
-		return err
-	}
-	bs, err := storeFromContext(ctx)
-	if err != nil {
-		return err
-	}
-	return bs.List(ctx, start, func(key string) error {
-		if c.Raw {
-			fmt.Println(key)
-		} else {
-			fmt.Printf("%x\n", key)
+	Run: func(ctx *command.Context, args []string) error {
+		if len(args) != 1 {
+			return errors.New("usage is: get <key>")
 		}
+		key, err := parseKey(args[0])
+		if err != nil {
+			return err
+		}
+		bs, err := storeFromContext(ctx)
+		if err != nil {
+			return err
+		}
+		data, err := bs.Get(getContext(ctx), key)
+		if err != nil {
+			return err
+		}
+		_, err = os.Stdout.Write(data)
+		return err
+	},
+}
+
+func init() {
+	putCmd.Flags.Bool("replace", false, "Replace an existing key")
+}
+
+var putCmd = &command.C{
+	Name:  "put",
+	Usage: "put <key> [<path>]",
+	Help:  "Write a blob to the store",
+
+	Run: func(ctx *command.Context, args []string) error {
+		if len(args) == 0 || len(args) > 2 {
+			return errors.New("usage is: put <key> [<path>]")
+		}
+		key, err := parseKey(args[0])
+		if err != nil {
+			return err
+		}
+		bs, err := storeFromContext(ctx)
+		if err != nil {
+			return nil
+		}
+		data, err := readData(getContext(ctx), "put", args[1:])
+		if err != nil {
+			return err
+		}
+
+		return bs.Put(getContext(ctx), blob.PutOptions{
+			Key:     key,
+			Data:    data,
+			Replace: getFlag(ctx, "replace").(bool),
+		})
+	},
+}
+
+var sizeCmd = &command.C{
+	Name:  "size",
+	Usage: "size <key>",
+	Help:  "Print the size of a stored blob",
+
+	Run: func(ctx *command.Context, args []string) error {
+		if len(args) != 1 {
+			return errors.New("usage is: size <key>")
+		}
+		key, err := parseKey(args[0])
+		if err != nil {
+			return err
+		}
+		bs, err := storeFromContext(ctx)
+		if err != nil {
+			return err
+		}
+		size, err := bs.Size(getContext(ctx), key)
+		if err != nil {
+			return err
+		}
+		fmt.Println(size)
 		return nil
-	})
+	},
 }
 
-type lenCmd struct{}
+var delCmd = &command.C{
+	Name:  "delete",
+	Usage: "delete <key>",
+	Help:  "Delete a blob from the store",
 
-func (lenCmd) Run(ctx context.Context, args []string) error {
-	if len(args) != 0 {
-		return errors.New("usage is: len")
-	}
-	bs, err := storeFromContext(ctx)
-	if err != nil {
-		return err
-	}
-	n, err := bs.Len(ctx)
-	if err != nil {
-		return err
-	}
-	fmt.Println(n)
-	return nil
+	Run: func(ctx *command.Context, args []string) error {
+		if len(args) != 1 {
+			return errors.New("usage is: delete <key>")
+		}
+		key, err := parseKey(args[0])
+		if err != nil {
+			return err
+		}
+		bs, err := storeFromContext(ctx)
+		if err != nil {
+			return err
+		}
+		del, ok := bs.(blob.Deleter)
+		if !ok {
+			return errors.New("store does not support deletion")
+		}
+		return del.Delete(getContext(ctx), key)
+	},
 }
 
-type casGroup struct {
-	Hash string `flag:"hash,CAS hash algorithm"`
-
-	Key casKeyCmd `vocab:"key" help-summary:"Compute the key for a blob without writing it"`
-	Put casPutCmd `vocab:"put" help-summary:"Write a content-addressed blob to the store"`
+func init() {
+	listCmd.Flags.Bool("raw", false, "Print raw keys without hex encoding")
+	listCmd.Flags.String("start", "", "List keys lexicographically greater than or equal to this")
 }
 
-func (c *casGroup) Init(ctx context.Context, name string, args []string) (context.Context, error) {
-	return context.WithValue(ctx, casKey{}, c), nil
+var listCmd = &command.C{
+	Name:  "list",
+	Usage: "list",
+	Help:  "List keys in the store",
+
+	Run: func(ctx *command.Context, args []string) error {
+		if len(args) != 0 {
+			return errors.New("usage is: list")
+		}
+		start, err := parseKey(getFlag(ctx, "start").(string))
+		if err != nil {
+			return err
+		}
+		bs, err := storeFromContext(ctx)
+		if err != nil {
+			return err
+		}
+		return bs.List(getContext(ctx), start, func(key string) error {
+			if getFlag(ctx, "raw").(bool) {
+				fmt.Println(key)
+			} else {
+				fmt.Printf("%x\n", key)
+			}
+			return nil
+		})
+	},
+}
+
+var lenCmd = &command.C{
+	Name:  "len",
+	Usage: "len",
+	Help:  "Print the number of stored keys",
+
+	Run: func(ctx *command.Context, args []string) error {
+		if len(args) != 0 {
+			return errors.New("usage is: len")
+		}
+		bs, err := storeFromContext(ctx)
+		if err != nil {
+			return err
+		}
+		n, err := bs.Len(getContext(ctx))
+		if err != nil {
+			return err
+		}
+		fmt.Println(n)
+		return nil
+	},
+}
+
+func init() {
+	casGroup.Flags.String("hash", "1", "CAS hash algorithm")
+}
+
+var casGroup = &command.C{
+	Name: "cas",
+	Help: "Manipulate a content-addressable blob store",
+
+	Init: func(ctx *command.Context) error {
+		cfg := ctx.Config.(*settings)
+		cfg.Hash = getFlag(ctx, "hash").(string)
+		return nil
+	},
+
+	Commands: []*command.C{
+		casKeyCmd,
+		casPutCmd,
+	},
 }
 
 func readData(ctx context.Context, cmd string, args []string) (data []byte, err error) {
@@ -216,79 +249,106 @@ func readData(ctx context.Context, cmd string, args []string) (data []byte, err 
 	return
 }
 
-type casPutCmd struct{}
+var casPutCmd = &command.C{
+	Name:  "put",
+	Usage: "put",
+	Help:  "Write a content-addressed blob to the store",
 
-func (casPutCmd) Run(ctx context.Context, args []string) error {
-	cas, err := casFromContext(ctx)
-	if err != nil {
-		return err
-	}
-	data, err := readData(ctx, "put", args)
-	if err != nil {
-		return err
-	}
-	key, err := cas.PutCAS(ctx, data)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("%x\n", key)
-	return nil
+	Run: func(ctx *command.Context, args []string) error {
+		cas, err := casFromContext(ctx)
+		if err != nil {
+			return err
+		}
+		data, err := readData(getContext(ctx), "put", args)
+		if err != nil {
+			return err
+		}
+		key, err := cas.PutCAS(getContext(ctx), data)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%x\n", key)
+		return nil
+	},
 }
 
-type casKeyCmd struct{}
+var casKeyCmd = &command.C{
+	Name: "key",
+	Help: "Compute the key for a blob without writing it",
 
-func (casKeyCmd) Run(ctx context.Context, args []string) error {
-	data, err := readData(ctx, "key", args)
-	if err != nil {
-		return err
-	}
-	h, err := hashFromContext(ctx)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("%x\n", blob.NewCAS(nil, h).Key(data))
-	return nil
+	Run: func(ctx *command.Context, args []string) error {
+		data, err := readData(getContext(ctx), "key", args)
+		if err != nil {
+			return err
+		}
+		h, err := hashFromContext(ctx)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%x\n", blob.NewCAS(nil, h).Key(data))
+		return nil
+	},
 }
 
-type tool struct {
-	Addr    string `flag:"store,Blob store address (required)"`
-	KeyFile string `flag:"keyfile,Path of encryption key file"`
-
-	Get  getCmd  `vocab:"get" help-summary:"Read a blob from the store"`
-	Put  putCmd  `vocab:"put" help-summary:"Write a blob to the store"`
-	Size sizeCmd `vocab:"size" help-summary:"Print the size of a stored blob"`
-	Del  delCmd  `vocab:"delete,del,rm" help-summary:"Delete a blob from the store"`
-	List listCmd `vocab:"list,ls" help-summary:"List keys in the store"`
-	Len  lenCmd  `vocab:"len,length" help-summary:"Print the number of stored keys"`
-
-	CAS  casGroup   `vocab:"cas" help-summary:"Manipulate a content-addressable blob store"`
-	Help vocab.Help `vocab:"help"`
-
-	_ struct{} `help-summary:"Manipulate the contents of a blob store"`
-	_ struct{} `help-long:"To specify blob keys literally, prefix them with @. To escape a leading @, double it.\nPrefix a base64-encoded key with \"+\". Otherwise, keys must be encoded in hexadecimal."`
+func init() {
+	tool.Flags.String("store", "", "store,Blob store address (required)")
+	tool.Flags.String("keyfile", os.Getenv("KEYFILE_PATH"), "Path of encryption key file")
 }
 
-func (t *tool) Init(ctx context.Context, name string, args []string) (context.Context, error) {
-	return context.WithValue(ctx, toolKey{}, t), nil
+type settings struct {
+	Context context.Context
+	Keyfile string
+	Store   string
+	Hash    string
 }
 
-type toolKey struct{}
+var tool = &command.C{
+	Name: filepath.Base(os.Args[0]),
+	Help: `Manipulate the contents of a blob store.
 
-func storeFromContext(ctx context.Context) (blob.Store, error) {
-	t := ctx.Value(toolKey{}).(*tool)
-	if t.Addr == "" {
+To specify blob keys literally, prefix them with @.
+To escape a leading @, double it.
+Prefix a base64-encoded key with "+".
+Otherwise, keys must be encoded in hexadecimal.
+`,
+
+	Init: func(ctx *command.Context) error {
+		ctx.Config = &settings{
+			Context: context.Background(),
+			Keyfile: getFlag(ctx, "keyfile").(string),
+			Store:   getFlag(ctx, "store").(string),
+		}
+		return nil
+	},
+
+	Commands: []*command.C{
+		getCmd, putCmd, sizeCmd, delCmd, listCmd, lenCmd, casGroup,
+		{
+			Name:  "help",
+			Usage: "[topic/command]",
+			Help:  "Print help for the specified command or topic",
+
+			CustomFlags: true,
+			Run:         command.RunHelp,
+		},
+	},
+}
+
+func storeFromContext(ctx *command.Context) (blob.Store, error) {
+	t := ctx.Config.(*settings)
+	if t.Store == "" {
 		return nil, errors.New("no -store address was specified")
 	}
-	st, err := store.Default.Open(ctx, t.Addr)
+	st, err := store.Default.Open(t.Context, t.Store)
 	if err != nil {
 		return nil, err
 	}
-	if t.KeyFile != "" {
+	if t.Keyfile != "" {
 		pp, err := getpass.Prompt("Passphrase: ")
 		if err != nil {
 			return nil, fmt.Errorf("reading passphrase: %v", err)
 		}
-		key, err := keyfile.LoadKey(t.KeyFile, pp)
+		key, err := keyfile.LoadKey(t.Keyfile, pp)
 		if err != nil {
 			return nil, fmt.Errorf("loading encryption key: %v", err)
 		}
@@ -301,9 +361,7 @@ func storeFromContext(ctx context.Context) (blob.Store, error) {
 	return st, err
 }
 
-type casKey struct{}
-
-func casFromContext(ctx context.Context) (blob.CAS, error) {
+func casFromContext(ctx *command.Context) (blob.CAS, error) {
 	h, err := hashFromContext(ctx)
 	if err != nil {
 		return blob.CAS{}, err
@@ -315,8 +373,8 @@ func casFromContext(ctx context.Context) (blob.CAS, error) {
 	return blob.NewCAS(bs, h), nil
 }
 
-func hashFromContext(ctx context.Context) (func() hash.Hash, error) {
-	c := ctx.Value(casKey{}).(*casGroup)
+func hashFromContext(ctx *command.Context) (func() hash.Hash, error) {
+	c := ctx.Config.(*settings)
 	switch c.Hash {
 	case "1", "sha1":
 		return sha1.New, nil
@@ -344,4 +402,13 @@ func parseKey(s string) (string, error) {
 		return "", fmt.Errorf("invalid key %q: %w", s, err)
 	}
 	return string(key), nil
+}
+
+func getFlag(ctx *command.Context, name string) interface{} {
+	v := ctx.Command.Flags.Lookup(name).Value
+	return v.(flag.Getter).Get()
+}
+
+func getContext(ctx *command.Context) context.Context {
+	return ctx.Config.(*settings).Context
 }
