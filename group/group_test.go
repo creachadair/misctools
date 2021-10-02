aa1e40db1b6bc6a16861b0c64e5ad1932c05c8eb
M. J. Fromberger <michael.j.fromberger@gmail.com>
2021-10-01 19:03:25 -0700

    group: Move test to its own package.

--- group/group_test.go
+++ group/group_test.go
@@ -1,11 +1,15 @@
-package group
+package group_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/creachadair/misctools/group"
+)
 
 func TestLookup(t *testing.T) {
 	const name = "nobody"
 
-	g1, err := Lookup(name)
+	g1, err := group.Lookup(name)
 	if err != nil {
 		t.Fatalf("Lookup(%q): unexpected error: %v", name, err)
 	}
@@ -14,12 +18,20 @@ func TestLookup(t *testing.T) {
 		t.Errorf("Lookup(%q): got name %q, want %q", name, g1.Name, name)
 	}
 
-	g2, err := LookupID(g1.ID)
+	g2, err := group.LookupID(g1.ID)
 	if err != nil {
 		t.Fatalf("LookupID(%d): unexpected error: %v", g1.ID, err)
 	}
 	t.Logf("LookupID(%d): %#v", g1.ID, g2)
 	if g2.ID != g1.ID {
 		t.Errorf("LookupID(%d): got id %d, want %d", g1.ID, g2.ID, g1.ID)
+	}
+
+	const noSuchUser = "no-such-user-nohow"
+	g3, err := group.Lookup(noSuchUser)
+	if err == nil {
+		t.Errorf("Lookup(%q): got %v, want error", noSuchUser, g3)
+	} else {
+		t.Logf("Lookup(%q): got expected error: %v", noSuchUser, err)
 	}
 }
