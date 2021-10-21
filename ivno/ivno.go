e1587e77c0b6a704a8a1675c93485a269a380644
M. J. Fromberger <michael.j.fromberger@gmail.com>
2021-10-21 08:20:56 -0700

    Allow revision of an existing serial.

--- ivno/ivno.go
+++ ivno/ivno.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -10,8 +11,13 @@ var revNo = flag.Int("rev", 0, "Revision number (0 for initial)")
 
 func main() {
 	flag.Parse()
-	ts := time.Now().In(time.UTC).Format("20060102150405")
-	base := "S" + ts + "Z"
+	var base string
+	if flag.NArg() == 0 {
+		ts := time.Now().In(time.UTC).Format("200601021504")
+		base = "S" + ts + "Z"
+	} else {
+		base = strings.SplitN(flag.Arg(0), "R", 2)[0]
+	}
 	if *revNo > 0 {
 		fmt.Printf("%sR%d\n", base, *revNo)
 	} else {
