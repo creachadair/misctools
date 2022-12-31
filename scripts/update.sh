250578c70b1184cd513096265058a09aa00a4879
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-12-31 10:35:57 -0800

    Add documentation to the update script.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
-
+#
+# Update Go modules in designated subdirectories.
+#
+# To install: Copy or link this script into a directory with subdirectories
+# that contain Git repositories holding Go packages.
+#
+# In order to be updated, a subdirectory must contain a .go-update file at the
+# top level. This file is sourced into the the update script.
+#
+# -- Overridable definitions:
+#
+#    presubmit   -- run tests prior to pushing an update
+#                   Default: go mod check
+#
+# -- Environment:
+#
+#  MODTIME   -- how long between updates (default: 1 day)
+#
 set -euo pipefail
 
-: ${MODTIME:=30m}
+: ${MODTIME:=1d}
+
+unset CDPATH
 
 readonly wd="$(dirname $0)"
 readonly cf=".go-update"
@@ -14,6 +33,7 @@ find . -depth 2 -type f -name "$cf" -mtime +"$MODTIME" -print | \
     cut -d/ -f2 |  while read -r pkg ; do
     (
         cd "$pkg"
+        printf "<> \033[1;93m%s\033[0m\n" "$pkg"
         . "$cf"
         git pull
         find . -type f -name go.mod -print | while read -r mod ; do
